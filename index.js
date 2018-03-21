fc6b52cc68e337662bb0f15ec0b3f53cb182e5ae
泽荣 <foru.fy@gmail.com>
2018-03-21 10:07:24 +0800

    0.0.4

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 export default function pify(func) {
   function caller(options) {
     function handler(resolve, reject) {
-      func({
-        success: (...args) => resolve(...args),
-        fail: (...args) => reject(...args),
-        ...options
-      });
+      func(
+        Object.assign({
+          success: (...args) => resolve(...args),
+          fail: (...args) => reject(...args)
+        }, options)
+      );
     }
     return new Promise(handler);
   }
